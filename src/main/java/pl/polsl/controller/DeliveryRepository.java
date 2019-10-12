e28edff5801275e69b65afaff126bb2c7380b4e2
PawWaw <pawel.w@hotmail.com>
2019-10-12 19:28:40 +0200

    Basic backend

--- src/main/java/pl/polsl/controller/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-//package pl.polsl.controller;
-//
-//import pl.polsl.model.Delivery;
-//import org.springframework.data.mongodb.repository.MongoRepository;
-//
-//public interface DeliveryRepository extends MongoRepository<Delivery, String> {
-//    Delivery findByName(String name);
-//}
